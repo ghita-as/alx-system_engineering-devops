b40f21b52d50caad180dfe5d5c0ef3d1123306a1
ghita-as <ghita.as.amranisouhli@gmail.com>
2024-04-23 01:47:05 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,10 @@
 #!/usr/bin/pup
 # Install a especific version of flask (2.1.0)
-package {'flask': ensure   => '2.1.0', provider => 'pip3'}
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
+}
